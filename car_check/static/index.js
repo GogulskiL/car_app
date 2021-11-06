aad1e903c1a1d162d859ef7f4c6917727f9b3a9a
gogul <lukaszgogul@gmail.com>
2021-11-07 00:11:25 +0100

    new functiion

--- car_check/static/index.js
+++ car_check/static/index.js
@@ -1,4 +1,11 @@
+const boxi = document.querySelectorAll("#box");
+
+console.log(boxi)
+
+boxi.forEach(function (box) {
+    box.addEventListener("click", function (event) {
+      this.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    });
+  }); 
 
 
-
-alert("Działa");
